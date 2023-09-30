1569a15a537a35dd27d8123ed07f9b2c7e17103c
ohs <ohs6006@gmail.com>
2023-09-30 21:40:28 +0900

    Modify : 배포를 위해 OverView 페이지 데이터 수정

--- src/pages/OverView.tsx
+++ src/pages/OverView.tsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
-// import Title from "../components/common/Title";
-// import SubTitle from "../components/common/SubTitle";
-// import Banner from "../components/Overview/Banner";
-// import LatestMatches from "../components/Overview/LatestMatches";
-// import RankTable from "../components/Overview/RankTable";
+import Title from "../components/common/Title";
+import SubTitle from "../components/common/SubTitle";
+import Banner from "../components/Overview/Banner";
+import LatestMatches from "../components/Overview/LatestMatches";
+import RankTable from "../components/Overview/RankTable";
 import Profile from "../components/Overview/Profile";
-// import Live from "../components/Overview/Live";
-// import PlayerList from "../components/Overview/PlayerList";
-// import SectionHeader from "../components/Overview/SectionHeader";
-// import useLeagueId from "../hooks/useLeagueId";
-// import { selectUser } from "../features/user/userSlice";
-// import { useSelector } from "react-redux";
+import Live from "../components/Overview/Live";
+import PlayerList from "../components/Overview/PlayerList";
+import SectionHeader from "../components/Overview/SectionHeader";
+import useLeagueId from "../hooks/useLeagueId";
+import { selectUser } from "../features/user/userSlice";
+import { useSelector } from "react-redux";
 
 const OverViewWrapper = styled.div`
   position: relative;
@@ -24,34 +24,34 @@ const OverViewWrapper = styled.div`
   }
 `;
 
-// const LeftSideWrapper = styled.section`
-//   flex: 1;
-// `;
+const LeftSideWrapper = styled.section`
+  flex: 1;
+`;
 
-// const LeftSideTempWrapper = styled.div`
-//   width: 100%;
-//   display: flex;
-//   gap: 1rem;
-//   margin-top: 1rem;
+const LeftSideTempWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
 
-//   @media (max-width: 1280px) {
-//     flex-direction: column;
-//   }
-// `;
+  @media (max-width: 1280px) {
+    flex-direction: column;
+  }
+`;
 
-// const RightSideWrapper = styled.section`
-//   display: flex;
-//   flex-direction: column;
-//   position: relative;
-//   width: 35%;
-//   margin-top: 59px;
-//   gap: 1rem;
+const RightSideWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 35%;
+  margin-top: 59px;
+  gap: 1rem;
 
-//   @media (max-width: 1280px) {
-//     width: 100%;
-//     margin-top: 0;
-//   }
-// `;
+  @media (max-width: 1280px) {
+    width: 100%;
+    margin-top: 0;
+  }
+`;
 
 const ProfileWrapper = styled.div`
   position: fixed;
@@ -68,15 +68,15 @@ const ProfileWrapper = styled.div`
 `;
 
 export default function OverView() {
-  // const leagueId = useLeagueId();
-  // const user = useSelector(selectUser);
+  const leagueId = useLeagueId();
+  const user = useSelector(selectUser);
 
   return (
     <OverViewWrapper>
       <ProfileWrapper>
         <Profile />
       </ProfileWrapper>
-      {/* <LeftSideWrapper>
+      <LeftSideWrapper>
         <Title title={`어서오세요 ${user?.displayName}님 !`} />
         <SubTitle subtitle="Football Mania에 오신 것을 환영합니다." />
         <Banner />
@@ -90,7 +90,7 @@ export default function OverView() {
       <RightSideWrapper>
         <PlayerList title="최다 득점 선수" type="topscorers" />
         <PlayerList title="최다 도움 선수" type="topassists" />
-      </RightSideWrapper> */}
+      </RightSideWrapper>
     </OverViewWrapper>
   );
 }
